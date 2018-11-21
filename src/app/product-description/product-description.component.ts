21dbd7123de253f229d5ca51da757bb1ceb77b9a
Korneliusz Buczkowski <Kornelio96@gmail.com>
2018-11-21 18:28:15 +0100

    fix - remove access modifier, add response import

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service';
     styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-    public albumInfo;
+    albumInfo;
 
     constructor(private _productService: ProductService) {
     }
